d3dde5c99414fc05bb0af2e358cdfba180f4ce08
Erik Nilsen <enilsen16@live.com>
2016-11-23 10:45:33 -0800

    distributed pubsub

--- lib/pub_sub.ex
+++ lib/pub_sub.ex
@@ -8,6 +8,17 @@ defmodule PubSub do
   end
 
   def broadcast(topic, msg) do
+    remote_broadcast(topic, msg)
+    local_broadcast(topic, msg)
+  end
+
+  def remote_broadcast(topic, msg) do
+    for node <- Node.list do
+      send({PubSub.Remote, node}, {:broadcast, topic, msg})
+    end
+  end
+
+  def local_broadcast(topic, msg) do
     Registry.dispatch(PubSub.Registry, topic, fn pids_with_values ->
       for {pid, _} <- pids_with_values do
         send(pid, msg)
--- lib/pub_sub/application.ex
+++ lib/pub_sub/application.ex
@@ -1,3 +1,16 @@
+defmodule PubSub.Remote do
+  use GenServer
+
+  def start_link do
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def handle_info({:broadcast, topic, message}, state) do
+    PubSub.local_broadcast(topic, message)
+    {:noreply, state}
+  end
+end
+
 defmodule PubSub.Application do
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
@@ -11,14 +24,15 @@ defmodule PubSub.Application do
 
     # Define workers and child supervisors to be supervised
     children = [
-      supervisor(Registry, [:duplicate, PubSub.Registry, registry_opts])
+      supervisor(Registry, [:duplicate, PubSub.Registry, registry_opts]),
+      worker(PubSub.Remote, [])
       # Starts a worker by calling: PubSub.Worker.start_link(arg1, arg2, arg3)
       # worker(PubSub.Worker, [arg1, arg2, arg3]),
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: PubSub.Supervisor]
+    opts = [strategy: :one_for_all, name: PubSub.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
