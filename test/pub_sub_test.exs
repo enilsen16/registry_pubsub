0cdf1988a9fa8040774d392cfd0de2c986785932
Erik Nilsen <enilsen16@live.com>
2016-11-23 11:09:56 -0800

    fix tests

--- test/pub_sub_test.exs
+++ test/pub_sub_test.exs
@@ -8,12 +8,27 @@ defmodule PubSubTest do
     assert_received :world
   end
 
-  test "subscribes and broadcasts with multiple" do
+  test "double subscription" do
     PubSub.subscribe("hello")
-    Task.start_link( fn ->
+    PubSub.subscribe("hello")
+    PubSub.broadcast("hello", :world)
+    assert_received :world
+    assert_received :world
+  end
+
+  test "does not receive messages from other topics" do
+    PubSub.subscribe("hello")
+    PubSub.broadcast("unknown", :world)
+    refute_received :world
+  end
+
+  test "subscribes and broadcasts with multiple processes" do
+    PubSub.subscribe("hello")
+
+    Task.start_link(fn ->
       PubSub.broadcast("hello", :world)
     end)
 
-    assert_received :world
+    assert_receive :world
   end
 end
